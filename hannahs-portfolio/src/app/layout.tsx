674d813f1badea605f498cfcb90487d9e8eb6867
Hannah Lindbäck <hannahlindback@gmail.com>
2025-05-12 10:56:56 +0200

    Implemented analytics from vercel

--- hannahs-portfolio/src/app/layout.tsx
+++ hannahs-portfolio/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Caveat } from "next/font/google";
 import "./globals.css";
 import TopNav from "./components/TopNav";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
             <TopNav />
         {children}
+        <Analytics />
      
       </body>
     </html>
